9157b35d540020d619919bc119760cc5529ba89c
Santos <wjdrms1919@gmail.com>
2022-01-15 00:28:00 +0900

    feat: 환경설정중#5

--- babel.config.js
+++ babel.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-	presets: [
-		'module:metro-react-native-babel-preset',
-		'@babel/preset-react',
-		'@babel/preset-typescript',
-	],
-	plugins: [
-		[
-			'module-resolver',
-			{
-				root: ['.'],
-				extensions: [
-					'.ios.ts',
-					'.android.ts',
-					'.ts',
-					'.ios.tsx',
-					'.android.tsx',
-					'.tsx',
-					'.jsx',
-					'.js',
-					'.json',
-				],
-				alias: {
-					'~': './src',
-					'@assets': './src/assets',
-					'@components': './src/components',
-					'@containers': './src/containers',
-					'@hooks': './src/hooks',
-					'@lib': './src/lib',
-					'@page': './src/page',
-					'@types': './src/types',
-				},
-			},
-		],
-	],
+  presets: [
+    "module:metro-react-native-babel-preset",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["."],
+        extensions: [
+          ".ios.ts",
+          ".android.ts",
+          ".ts",
+          ".ios.tsx",
+          ".android.tsx",
+          ".tsx",
+          ".jsx",
+          ".js",
+          ".json",
+        ],
+        alias: {
+          "~": "./src",
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@containers": "./src/containers",
+          "@hooks": "./src/hooks",
+          "@lib": "./src/lib",
+          "@page": "./src/page",
+          "@types": "./src/types",
+        },
+      },
+    ],
+  ],
 };
