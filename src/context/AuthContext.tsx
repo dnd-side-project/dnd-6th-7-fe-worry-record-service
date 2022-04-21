0e2c0e3776db06d4a8e77a8e335ee1f9b19d40ee
Santos <wjdrms1919@gmail.com>
2022-04-21 23:57:09 +0900

    test: 코드수정

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -85,7 +85,7 @@ export function AuthProvider({
 
     // 이미 로그인이 되어 있는 상황
     console.log('이미 로그인이 되어 있는 상황');
-    const result = await authService.updateFCMToken({ userId, deviceToken });
+    await authService.updateFCMToken({ userId, deviceToken });
     SplashScreen.hide();
     setUser(true);
   }, [authService]);
